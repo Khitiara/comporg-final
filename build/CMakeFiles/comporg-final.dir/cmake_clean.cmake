6afb86354fca8dd62cd6530dad1ab01ec42bc206
Alex Sieberer <robotbrainify@gmail.com>
2018-04-18 12:31:02 -0400

    Initial path structure

---
+++ build/CMakeFiles/comporg-final.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comporg-final"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/comporg-final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/impl2/CMakeFiles/impl2.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/impl2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
