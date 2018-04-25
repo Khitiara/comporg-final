ffbb2bc992da232137e1619b2573b808773eabe6
Evelyn Schneider <evelyn.eliz.schneider@gmail.com>
2018-04-25 15:48:14 -0400

    Create the individual files

---
+++ src/base.h
@@ -0,0 +1,110 @@
+// vim: ft=c
+
+#ifndef BASE_H
+#define BASE_H
+
+#include <stdlib.h>
+
+#define MAX_CACHE_SIZE 10240
+#define CACHE_MISS_DELAY 10 // 10 cycle cache miss penalty
+#define MAX_STAGES 5
+
+void iplc_sim_init(int index, int blocksize, int assoc);
+
+// Outout performance results
+void iplc_sim_finalize();
+
+typedef struct cache_line
+{
+    // Your data structures for implementing your cache should include:
+    // a valid bit
+    // a tag
+    // a method for handling varying levels of associativity
+    // a method for selecting which item in the cache is going to be replaced
+} cache_line_t;
+
+cache_line_t *cache=NULL;
+int cache_index=0;
+int cache_blocksize=0;
+int cache_blockoffsetbits = 0;
+int cache_assoc=0;
+long cache_miss=0;
+long cache_access=0;
+long cache_hit=0;
+
+char instruction[16];
+char reg1[16];
+char reg2[16];
+char offsetwithreg[16];
+unsigned int data_address=0;
+unsigned int instruction_address=0;
+unsigned int pipeline_cycles=0;   // how many cycles did you pipeline consume
+unsigned int instruction_count=0; // home many real instructions ran thru the pipeline
+unsigned int branch_predict_taken=0;
+unsigned int branch_count=0;
+unsigned int correct_branch_predictions=0;
+
+unsigned int debug=0;
+unsigned int dump_pipeline=1;
+
+enum instruction_type {NOP, RTYPE, LW, SW, BRANCH, JUMP, JAL, SYSCALL};
+
+typedef struct rtype
+{
+    char instruction[16];
+    int reg1;
+    int reg2_or_constant;
+    int dest_reg;
+    
+} rtype_t;
+
+typedef struct load_word
+{
+    unsigned int data_address;
+    int dest_reg;
+    int base_reg;
+    
+} lw_t;
+
+typedef struct store_word
+{
+    unsigned int data_address;
+    int src_reg;
+    int base_reg;
+} sw_t;
+
+typedef struct branch
+{
+    int reg1;
+    int reg2;
+    
+} branch_t;
+
+
+typedef struct jump
+{
+    char instruction[16];
+    
+} jump_t;
+
+typedef struct pipeline
+{
+    enum instruction_type itype;
+    unsigned int instruction_address;
+    union
+    {
+        rtype_t   rtype;
+        lw_t      lw;
+        sw_t      sw;
+        branch_t  branch;
+        jump_t    jump;
+    }
+    stage;
+    
+} pipeline_t;
+
+enum pipeline_stages {FETCH, DECODE, ALU, MEM, WRITEBACK};
+
+pipeline_t pipeline[MAX_STAGES];
+
+#endif
---
+++ src/coder2.h
@@ -0,0 +1,10 @@
+// vim: ft=c
+
+#ifndef CODER2_H
+#define CODER2_H
+
+#include "base.h"
+
+void iplc_sim_push_pipeline_stage();
+
+#endif
