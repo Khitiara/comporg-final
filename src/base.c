156c2bd07e9f0f9c036385d05a3abf866a90a4cd
Chris Nero <chrisvn00@gmail.com>
2018-04-27 20:54:34 -0400

    Fix multi definition issue when compiling all files

---
+++ src/base.c
@@ -0,0 +1,25 @@
+#include "base.h"
+
+cache_line_t *cache=NULL;
+int cache_index=0;
+int cache_blocksize=0;
+int cache_blockoffsetbits = 0;
+int cache_assoc=0;
+long cache_miss=0;
+long cache_access=0;
+long cache_hit=0;
+
+char instruction[16];
+char reg1[16];
+char reg2[16];
+char offsetwithreg[16];
+unsigned int data_address=0;
+unsigned int instruction_address=0;
+unsigned int pipeline_cycles=0;   // how many cycles did you pipeline consume
+unsigned int instruction_count=0; // home many real instructions ran thru the pipeline
+unsigned int branch_predict_taken=0;
+unsigned int branch_count=0;
+unsigned int correct_branch_predictions=0;
+
+unsigned int debug=0;
+unsigned int dump_pipeline=1;
