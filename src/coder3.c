c7a91e765d7d85d13335fb1397b184845f7bef09
Nate Bennett <nate.f.bennett@gmail.com>
2018-04-30 02:43:04 -0400

    comments added to process pipeline

--- src/coder3.c
+++ src/coder3.c
@@ -10,11 +10,13 @@
 void iplc_sim_process_pipeline_rtype(char *instruction, int dest_reg, int reg1, int reg2_or_constant)
 {
     /* This is an example of what you need to do for the rest */
-    iplc_sim_push_pipeline_stage();
+    // assign vals to instruction specific vars();
     
-    pipeline[FETCH].itype = RTYPE;
+    // assign vals to itype + instruction_address
+    pipeline[FETCH].itype = RTYPE; 
     pipeline[FETCH].instruction_address = instruction_address;
     
+    // assign vals to instruction specific vars
     strcpy(pipeline[FETCH].stage.rtype.instruction, instruction);
     pipeline[FETCH].stage.rtype.reg1 = reg1;
     pipeline[FETCH].stage.rtype.reg2_or_constant = reg2_or_constant;
@@ -25,9 +27,11 @@ void iplc_sim_process_pipeline_lw(int dest_reg, int base_reg, unsigned int data_
 {
     iplc_sim_push_pipeline_stage();
 
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = LW;
     pipeline[FETCH].instruction_address = instruction_address;
 
+    // assign vals to instruction specific vars
     pipeline[FETCH].stage.lw.data_address = data_address;
     pipeline[FETCH].stage.lw.dest_reg = dest_reg;
     pipeline[FETCH].stage.lw.base_reg = base_reg;
@@ -37,9 +41,11 @@ void iplc_sim_process_pipeline_sw(int src_reg, int base_reg, unsigned int data_a
 {
     iplc_sim_push_pipeline_stage();
 
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = SW;
     pipeline[FETCH].instruction_address = instruction_address;
 
+    // assign vals to instruction specific vars
     pipeline[FETCH].stage.sw.data_address = data_address;
     pipeline[FETCH].stage.sw.src_reg = src_reg;
     pipeline[FETCH].stage.sw.base_reg = base_reg;
@@ -49,9 +55,11 @@ void iplc_sim_process_pipeline_branch(int reg1, int reg2)
 {
     iplc_sim_push_pipeline_stage();
 
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = BRANCH;
     pipeline[FETCH].instruction_address = instruction_address;
 
+    // assign vals to instruction specific vars
     pipeline[FETCH].stage.branch.reg1 = reg1;
     pipeline[FETCH].stage.branch.reg2 = reg2;
 }
@@ -60,9 +68,11 @@ void iplc_sim_process_pipeline_jump(char *instruction)
 {
     iplc_sim_push_pipeline_stage();
 
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = JUMP;
     pipeline[FETCH].instruction_address = instruction_address;
     
+    // assign vals to instruction specific vars
     strcpy(pipeline[FETCH].stage.jump.instruction, instruction);
 }
 
@@ -70,6 +80,7 @@ void iplc_sim_process_pipeline_syscall()
 {
     iplc_sim_push_pipeline_stage();
 
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = SYSCALL;
     pipeline[FETCH].instruction_address = instruction_address;
 }
@@ -78,6 +89,7 @@ void iplc_sim_process_pipeline_nop()
 {
     iplc_sim_push_pipeline_stage();
     
+    // assign vals to itype + instruction_address
     pipeline[FETCH].itype = NOP;
     pipeline[FETCH].instruction_address = 0;
 }
