cffb5c99cb8fae75c2015869cd4af42027255c15
Sean Lossef <seanlossef@gmail.com>
2018-04-30 03:44:33 -0400

    LRU Hit and Miss

--- src/coder1.c
+++ src/coder1.c
@@ -6,7 +6,16 @@
  */
 void iplc_sim_LRU_replace_on_miss(int index, int tag)
 {
-    /* You must implement this function */
+    // replace least recent cache spot
+    int replace_index = cache[index].replace[0];
+
+    cache[index].tag[replace_index] = tag;
+    cache[index].valid_bit[replace_index] = 1;
+    cache[index].replace[cache_assoc - 1] = replace_index;
+    
+    // translate cache array at index
+    for(int i = 0; i < cache_assoc - 1; i++)
+        cache[index].replace[i] = cache[index].replace[i + 1];
 }
 
 /*
@@ -15,7 +24,16 @@ void iplc_sim_LRU_replace_on_miss(int index, int tag)
  */
 void iplc_sim_LRU_update_on_hit(int index, int assoc_entry)
 {
-    /* You must implement this function */
+	// search for update index
+    int update = 0;
+    while(cache[index].replace[update] != assoc_entry)
+        update++;
+
+    // translate lru over
+    for(int i = update + 1; i < cache_assoc; i++)
+        cache[index].replace[i - 1] = cache[index].replace[i];
+    
+    cache[index].replace[cache_assoc - 1] = assoc_entry;
 }
 
 /*
@@ -26,12 +44,43 @@ void iplc_sim_LRU_update_on_hit(int index, int assoc_entry)
  */
 int iplc_sim_trap_address(unsigned int address)
 {
-    int i=0, index=0;
-    int tag=0;
-    int hit=0;
+    int i = 0
+    int index = 0;
+    int tag = 0;
+    int hit = 0;
     
-    // Call the appropriate function for a miss or hit
+    unsigned int filter;
 
-    /* expects you to return 1 for hit, 0 for miss */
+    // find index in address
+    filter = (1 << (cache_blockoffsetbits + cache_index))--;
+    index = address & filter;
+    index = index >> cache_blockoffsetbits;
+
+    // find tag in address
+    filter = (1 << (cache_blockoffsetbits + cache_index + cache_assoc + 16))--;
+    tag = address & filter;
+    tag = tag >> (cache_blockoffsetbits + cache_index);
+
+    // check if tag is in cache
+    for(i = 0; i < cache_assoc; i++){
+        if ((cache[index].valid_bit[i] == 1) && (cache[index].tag[i] == tag)) {
+            hit = 1;
+            break;
+        }
+    }
+
+    // access cache
+    printf("Address %x: Tag= %x, Index= %x\n", address,tag,index);
+    cache_access++;
+
+    // hit or miss functions
+    if (hit == 1){
+        cache_hit++;
+        iplc_sim_LRU_update_on_hit(index, i);
+    } else {
+        cache_miss++;
+        iplc_sim_LRU_replace_on_miss(index, tag);
+    }
+
     return hit;
 }
